4a0ee313bfc3506a68eda35ca192fa1ed40ba449
sanram03 <35326328+sanram03@users.noreply.github.com>
2018-06-11 16:33:58 +0530

    Update ViewController.swift

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Hello Github")
+        print("Pull Request")
     }
 
     override func didReceiveMemoryWarning() {
