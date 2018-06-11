29291a7c59484e0ad76cf537c9f957d6fe000622
Rim global <rimglobal@rims-imac.local>
2018-06-11 17:09:05 +0530

    abc

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Push Requestt")
+        print("Pushing Requestt")
     }
 
     override func didReceiveMemoryWarning() {
