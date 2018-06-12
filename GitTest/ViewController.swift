94c830d8cdedfcc9479931e284eae7f2aaa269fc
sanram03 <35326328+sanram03@users.noreply.github.com>
2018-06-12 10:27:31 +0530

    Update ViewController.swift

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Pushing Requestt..branch1")
+        print("Pushing Requestt..")
     }
 
     override func didReceiveMemoryWarning() {
